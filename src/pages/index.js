e4b7c0d2f302ee21efbb0625bb090b4301f8ed3c
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2021-10-13 16:00:44 -0400

    added stuff to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.css'
 import Header from '../components/header';
-import { Container } from 'semantic-ui-react'
+import { Container, Segment, Button } from 'semantic-ui-react'
 
 const HomePage = () => {
  return (
     <React.Fragment>
       <Container style={{padding:50}}>
         <Header></Header>
+        <Segment textAlign="center">
+          <h1 style={{marginBottom:20}}> HOME </h1>
+          <p style={{marginBottom:20}}> Some random text that needs not readen.</p>
+          <Button color="green" style={{marginBottom:20}}>Join Now!</Button>
+        </Segment>
       </Container>
     </React.Fragment>
  );
