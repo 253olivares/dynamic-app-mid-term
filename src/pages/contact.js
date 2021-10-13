f6c19d0c954296e9d7ded9e5b853a316faecf965
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2021-10-13 16:18:16 -0400

    added stuff to my contact but have not finished yet

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,55 @@
 import React from'react';
 import 'semantic-ui-css/semantic.css'
 import Header from '../components/header';
-import { Container } from 'semantic-ui-react'
+import { Container,Form,Input,TextArea,Button } from 'semantic-ui-react'
 //import {Link} from 'gatsby';
  
 const ContactPage = () => {
+
+    function consoleLogChange(e, {}){
+
+    }
+
+    function clearForm(){
+        
+    }
+
+
   return(
     <React.Fragment>
         <React.Fragment>
       <Container style={{padding:50}}>
         <Header></Header>
-        <h1>Contact</h1>
+        <h1>Contact Us!</h1>
+        <Form>
+            <Form.Field
+            control={Input}
+            label="Full Name:"
+            placeholder="Enter your full name..."
+            name="name"
+            >
+
+            </Form.Field>
+            <Form.Field
+            control={Input}
+            label="Contact Email:"
+            placeholder="Enter the best email to contact you with..."
+            name="email"
+            >
+                
+            </Form.Field>
+            <Form.Field
+            control={TextArea}
+            label="Message:"
+            placeholder="Enter your message..."
+            name="message"
+            >
+                
+            </Form.Field>
+            <Button.Group>
+                <Button color="green" onClick={clearForm}>Send Message</Button>
+            </Button.Group>
+        </Form>
       </Container>
     </React.Fragment>
     </React.Fragment>)
